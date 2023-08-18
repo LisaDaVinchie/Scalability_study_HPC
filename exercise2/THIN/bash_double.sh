bd3c9fa99e016fb018f632a92823c75c7d5fc6d9
Lisa <ldavin00@login02.hpc>
2023-08-18 16:53:17 +0200

    updates

--- exercise2/THIN/bash_double.sh
+++ exercise2/THIN/bash_double.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# requested nodes
+# Requested nodes
 #SBATCH --nodes=1
 
 # Tasks per node
@@ -9,26 +9,26 @@
 # Partition
 #SBATCH --partition=THIN
 
-# Time limit: 1h
-#SBATCH --time=1:0:0
+# Time limit: 2h
+#SBATCH --time=2:0:0
 
 # Load needen modules
 module load architecture/Intel
 module load mkl
-module load openBLAS/0.3.21-omp
+module load openBLAS/0.3.23-omp
 
 # choose matrix dimesion
-matrix_dim = 2000
+MATRIX_DIM=2000
 
 # run dgemm with double precision
 srun -n1 make double
 
 #execute requiring 24 cpus per task
-srun -n1 --cpus-per-task=24 ./gemm_mkl.exe $matrix_dim $matrix_dim $matrix_dim
+srun -n1 --cpus-per-task=24 ./gemm_mkl.exe $MATRIX_DIM $MATRIX_DIM $MATRIX_DIM
 
 
-srun -n1 --cpus-per-task=24 ./gemm_openblas.exe $matrix_dim $matrix_dim $matrix_dim
+srun -n1 --cpus-per-task=24 ./gemm_openblas.exe $MATRIX_DIM $MATRIX_DIM $MATRIX_DIM
 
 
-srun -n1 --cpus-per-task=24 ./gemm_blis.exe $matrix_dim $matrix_dim $matrix_dim
+srun -n1 --cpus-per-task=24 ./gemm_blis.exe $MATRIX_DIM $MATRIX_DIM $MATRIX_DIM
 
