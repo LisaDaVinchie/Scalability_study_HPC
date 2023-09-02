2de81df92f23844e63ef5f81dd3ba460781f4d15
Goldstone8000 <108172690+Goldstone8000@users.noreply.github.com>
2023-09-02 12:12:42 +0200

    fixed errors

--- exercise1/game_of_life_header.h
+++ exercise1/game_of_life_header.h
@@ -2,5 +2,5 @@ void write_pgm_image(char *image, int maxval, int xsize, int ysize, const char *
 void read_pgm_image( char **image, int *maxval, int *xsize, int *ysize, const char *image_name);
 void random_playground(char* image, int xsize, int ysize);
 int count_live_neighbors(char* image, int row, int col, int xsize, int ysize);
-int count_live_neighbors(char* image, int row, int col, int xsize, int ysize);
+void ordered_evolution(char* image, int xsize, int ysize, int n, int s, char *destination_folder);
 void static_evolution(char* image, int xsize, int ysize, int n, int s, char *destination_folder);
