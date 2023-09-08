b08bd97596c121245eccc08636264db026b5239e
Goldstone8000 <108172690+Goldstone8000@users.noreply.github.com>
2023-09-08 12:32:07 +0200

    Changed parallel code

--- exercise1/program/parallel_header.h
+++ exercise1/program/parallel_header.h
@@ -7,7 +7,7 @@ void read_header(int *xsize, int *ysize, int *maxval, char *filename);
 void read_pgm_image(unsigned char* image, int xsize, int ysize, int maxval, char* filename);
 unsigned char count_live_neighbors(unsigned char* image, int row, int col, int xsize, int ysize);
 void ordered_evolution(unsigned char* image, int xsize, int ysize, int n, int s, char *destination_folder);
-void static_upgrade(unsigned char* image, unsigned char* original_image, int xwidth, int ywidth, int x, int y);
+int static_upgrade(unsigned char* image, unsigned char* original_image, int xwidth, int ywidth, int x, int y);
 void save_snapshot(unsigned char* image, int xwidth, int ywidth, int maxval, char* snap_title, int image_idx);
 
 #endif
