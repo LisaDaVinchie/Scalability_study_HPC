a68d47f4ec1b130d1fd7c144f6a290b5dfee5449
Goldstone8000 <108172690+Goldstone8000@users.noreply.github.com>
2023-09-08 14:55:52 +0200

    Changes

--- exercise1/program/game_of_life_parallel.c
+++ exercise1/program/game_of_life_parallel.c
@@ -485,13 +485,16 @@ int main ( int argc, char **argv )
             for (x = 0; x < xwidth; x++){
               printf("Rank %d, started y cycle %d, starting x cycle %d\n", rank, y, x);
               int check = static_upgrade(image, original_image, xwidth, ywidth, x, y);
-
+              printf("%d ", (int)image[x + y * xwidth]);
+              
               if(check != 0){
                 MPI_Abort(MPI_COMM_WORLD, 1);
               }
             }
+            printf("\n");
           }
           printf("Ended upgrade\n");
+          
 
           // MPI_Barrier(MPI_COMM_WORLD);
 
